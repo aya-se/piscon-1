2f4df7baf7b646afe6856c7b73b16d0dbf2c3e98
aya-se <kakkeno1@gmail.com>
2022-07-02 01:44:26 +0900

    bulk insertの追加(試験的)

--- go/main.go
+++ go/main.go
@@ -90,6 +90,15 @@ type IsuCondition struct {
 	CreatedAt  time.Time `db:"created_at"`
 }
 
+// INSERT用のIsuCondition型
+type IsuConditionInsert struct {
+	JIAIsuUUID string    `db:"jia_isu_uuid"`
+	Timestamp  time.Time `db:"timestamp"`
+	IsSitting  bool      `db:"is_sitting"`
+	Condition  string    `db:"condition"`
+	Message    string    `db:"message"`
+}
+
 type MySQLConnectionEnv struct {
 	Host     string
 	Port     string
@@ -1195,25 +1204,43 @@ func postIsuCondition(c echo.Context) error {
 		return c.String(http.StatusNotFound, "not found: isu")
 	}
 
-	for _, cond := range req {
+	// bulk insertの実装
+	conditions := make([]IsuConditionInsert, len(req))
+
+	for i, cond := range req {
 		timestamp := time.Unix(cond.Timestamp, 0)
 
 		if !isValidConditionFormat(cond.Condition) {
 			return c.String(http.StatusBadRequest, "bad request body")
 		}
 
-		_, err = tx.Exec(
-			"INSERT INTO `isu_condition`"+
-				"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
-				"	VALUES (?, ?, ?, ?, ?)",
-			jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
-		if err != nil {
-			c.Logger().Errorf("db error: %v", err)
-			return c.NoContent(http.StatusInternalServerError)
+		// bulk insertの実装
+		conditions[i] = IsuConditionInsert {
+			JIAIsuUUID: jiaIsuUUID,
+			Timestamp: timestamp,
+			IsSitting: cond.IsSitting,
+			Condition: cond.Condition,
+			Message: cond.Message,
 		}
+		//_, err = tx.Exec(
+		//	"INSERT INTO `isu_condition`"+
+		//		"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
+		//		"	VALUES (?, ?, ?, ?, ?)",
+		//	jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
+		// if err != nil {
+		// 	c.Logger().Errorf("db error: %v", err)
+		// 	return c.NoContent(http.StatusInternalServerError)
+		// }
 
 	}
-
+	query := "INSERT INTO `isu_condition`" +
+							"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)" +
+							"	VALUES (:conditions)"
+  _, err := tx.NamedExec(query, conditions)
+  if err != nil {
+    c.Logger().Errorf("db error: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+  }
 	err = tx.Commit()
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
